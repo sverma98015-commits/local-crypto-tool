414d611ddae79a0c97b065665559223aa7c7abaa
sverma98015-commits <sverma98015@gmail.com>
2026-06-22 12:28:05 +0530

    Update gui.py

--- ui/gui.py
+++ ui/gui.py
@@ -1,11 +1,73 @@
 import tkinter as tk
+from tkinter import messagebox
+
+from crypto.encryptor import encrypt_text
+from crypto.decryptor import decrypt_text
+
 
 def run_gui():
+
     root = tk.Tk()
     root.title("Local Crypto Tool")
-    root.geometry("600x400")
+    root.geometry("800x600")
 
-    label = tk.Label(root, text="Local Crypto Tool")
-    label.pack(pady=20)
+    tk.Label(root, text="Text").pack()
+
+    text_box = tk.Text(root, height=10)
+    text_box.pack(fill="x")
+
+    tk.Label(root, text="Password").pack()
+
+    password_entry = tk.Entry(root, show="*")
+    password_entry.pack(fill="x")
+
+    tk.Label(root, text="Result").pack()
+
+    result_box = tk.Text(root, height=10)
+    result_box.pack(fill="x")
+
+    def encrypt():
+
+        try:
+
+            result_box.delete("1.0", tk.END)
+
+            result = encrypt_text(
+                text_box.get("1.0", tk.END),
+                password_entry.get()
+            )
+
+            result_box.insert(tk.END, result)
+
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
+
+    def decrypt():
+
+        try:
+
+            result_box.delete("1.0", tk.END)
+
+            result = decrypt_text(
+                text_box.get("1.0", tk.END).strip(),
+                password_entry.get()
+            )
+
+            result_box.insert(tk.END, result)
+
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
+
+    tk.Button(
+        root,
+        text="Encrypt",
+        command=encrypt
+    ).pack(pady=10)
+
+    tk.Button(
+        root,
+        text="Decrypt",
+        command=decrypt
+    ).pack(pady=10)
 
     root.mainloop()
