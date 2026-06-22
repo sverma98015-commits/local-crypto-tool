be24fae70a75b16d5ffa838c4f168d6099cf53e7
sverma98015-commits <sverma98015@gmail.com>
2026-06-22 12:27:25 +0530

    Update hash_verifier.py

--- integrity/hash_verifier.py
+++ integrity/hash_verifier.py
@@ -1,2 +1,13 @@
-def calculate_hash():
-    pass
+import hashlib
+
+
+def calculate_hash(file_path):
+
+    sha256 = hashlib.sha256()
+
+    with open(file_path, "rb") as file:
+
+        while chunk := file.read(4096):
+            sha256.update(chunk)
+
+    return sha256.hexdigest()
