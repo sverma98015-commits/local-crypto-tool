1aaf815a0f7aed71e338e2e5b79ff0c90c4c537e
sverma98015-commits <sverma98015@gmail.com>
2026-06-22 12:30:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
-from ui.gui import run_gui
+from flask import Flask, render_template, request
+from crypto.encryptor import encrypt_text
+from crypto.decryptor import decrypt_text
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    result = ""
+
+    if request.method == "POST":
+
+        text = request.form["text"]
+        password = request.form["password"]
+
+        if request.form["action"] == "encrypt":
+            result = encrypt_text(text, password)
+
+        elif request.form["action"] == "decrypt":
+            result = decrypt_text(text, password)
+
+    return render_template("index.html", result=result)
 
 if __name__ == "__main__":
-    run_gui()
+    app.run(host="0.0.0.0", port=5000)
