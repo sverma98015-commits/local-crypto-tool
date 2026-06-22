7337323708ba547f7428dd12c434d11addbc54b3
sverma98015-commits <sverma98015@gmail.com>
2026-06-22 12:24:10 +0530

    Update key_manager.py

--- crypto/key_manager.py
+++ crypto/key_manager.py
@@ -1,2 +1,14 @@
-def derive_key():
-    pass
+import base64
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+def derive_key(password, salt):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
+
+    return kdf.derive(password.encode())
