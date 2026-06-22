edda1928e3c280f20e3c84591004fc62c629e5c4
sverma98015-commits <sverma98015@gmail.com>
2026-06-22 12:55:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+
 from crypto.encryptor import encrypt_text
 from crypto.decryptor import decrypt_text
 from integrity.hash_verifier import calculate_hash
@@ -9,19 +10,38 @@ app = Flask(__name__)
 def home():
 
     result = ""
+    file_hash = ""
 
     if request.method == "POST":
 
-        text = request.form["text"]
-        password = request.form["password"]
+        action = request.form.get("action")
 
-        if request.form["action"] == "encrypt":
+        if action == "encrypt":
+
+            text = request.form["text"]
+            password = request.form["password"]
+
             result = encrypt_text(text, password)
 
-        elif request.form["action"] == "decrypt":
+        elif action == "decrypt":
+
+            text = request.form["text"].strip()
+            password = request.form["password"]
+
             result = decrypt_text(text, password)
 
-    return render_template("index.html", result=result)
+        elif action == "hash":
+
+            uploaded_file = request.files["file"]
+
+            if uploaded_file:
+                file_hash = calculate_hash(uploaded_file)
+
+    return render_template(
+        "index.html",
+        result=result,
+        file_hash=file_hash
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
